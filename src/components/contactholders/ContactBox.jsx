1c76f1e6645f9ad89233d25849ccf8444d5cb49d
Dillan <dillyferster@gmail.com>
2024-05-31 16:47:03 -0600

    update

--- src/components/contactholders/ContactBox.jsx
+++ src/components/contactholders/ContactBox.jsx
@@ -1,22 +1,27 @@
 import React from "react";
 import { motion } from "framer-motion";
+import arrow from "../../assets/arrow.png";
 
 const ContactLink = ({ link }) => {
   return (
     <div className="flex flex-col  items-center  px-4">
       <div
         id="linkedinBox"
-        className=" w-[80%] max-w-[45rem] flex flex-col items-center  text-[4em] font-Armata  overflow-hidden max-sm:text-[3rem]"
+        className=" w-[80%] max-w-[45rem] flex flex-col items-center  text-[4rem] font-Armata  overflow-hidden max-sm:text-[3rem]"
       >
         <motion.div
-          className="flex w-[20rem] justify-start max-sm:w-[15rem] 
+          className="flex w-[22rem] justify-start max-sm:w-[16rem] 
         "
           whileHover={{ scale: 1.05 }}
           transition={{ type: "spring", stiffness: 100 }}
         >
           {link}
-          <div className=" flex flex-col justify-center text-[var(--color-purple)] text-[3rem]">
-            &#8599;
+          <div className=" flex flex-col justify-center">
+            <img
+              className="w-[3rem] max-sm:w-[2.5rem]"
+              src={arrow}
+              alt="arrow"
+            ></img>
           </div>
         </motion.div>
 
