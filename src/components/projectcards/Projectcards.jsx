9c27569c462c88958d97a821b21981e2f713cc81
Dillan <dillyferster@gmail.com>
2024-06-03 17:09:48 -0600

    ud

--- src/components/projectcards/Projectcards.jsx
+++ src/components/projectcards/Projectcards.jsx
@@ -7,9 +7,9 @@ const Projectcards = () => {
   const descYycFlights =
     "Web application that displays flight prices and locations for popular direct flights out of Calgary, it uses three separate API's flights data, city data, and images. All of the data and images are live, they will all automatically update with new destinations. When the select option from the flight details card is chosen it will re-direct to purchase the ticket. ";
   const techYycFlights = [
-    " React.js, Next.js, Tailwind",
+    " React.js, Next.js, CSS",
     <br />,
-    "Github, Vercel, fetchAPI",
+    "fetchAPI, Github",
   ];
 
   return (
