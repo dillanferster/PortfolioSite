58e1db810fed2aae7a73e7d65d24e027756f0300
Dillan <dillyferster@gmail.com>
2024-06-03 18:41:48 -0600

    ud

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -8,9 +8,9 @@ const NavBar = ({ navpage }) => {
   const navOptions = ["home", "bio", "projects", "contact"];
 
   return (
-    <div className=" sticky top-0 z-[1000] flex gap-4 justify-center items-center max-sm:gap-2 px-12 max-sm:px-6 max-md:px-10 cursor-pointer ">
+    <div className=" sticky top-0 z-[1000] flex gap-4 justify-center items-center max-sm:gap-2 px-12 max-sm:px-6 max-md:px-10 ">
       <div className=" backdrop-blur-md  flex items-center justify-between pl-8 mt-4 border-2 border-[#414558] bg-[var(--color-nav)] rounded-full h-14 w-[66rem]  min-w-[10.5rem] max-sm:pl-0  max-sm:justify-center  shadow-sm ">
-        <div className="font-BebasNeue text-3xl max-sm:text-lg text-gray-400">
+        <div className="font-BebasNeue text-3xl max-sm:text-lg text-gray-400 cursor-pointer ">
           <a href="#homeSection">Dillan Ferster</a>
         </div>
         {/* <div className="pr-8 max-sm:pr-6 max-sm:hidden">
@@ -19,7 +19,7 @@ const NavBar = ({ navpage }) => {
       </div>
       <div className="flex flex-col ">
         <div
-          className=" backdrop-blur-md  flex items-center justify-center border-2 border-[var(--color-secondary)]  rounded-full h-14 mt-4 w-[10rem] min-w-[10.5rem] bg-[var(--color-nav)] hover:bg-[var(--color-navHover)]  shadow-sm"
+          className=" backdrop-blur-md cursor-pointer  flex items-center justify-center border-2 border-[var(--color-secondary)]  rounded-full h-14 mt-4 w-[10rem] min-w-[10.5rem] bg-[var(--color-nav)] hover:bg-[var(--color-navHover)]  shadow-sm"
           onClick={() => setOpenNav(!openNav)}
         >
           <div className="flex justify-center items-center text-gray-400  w-[100%] px-8">
@@ -43,7 +43,10 @@ const NavBar = ({ navpage }) => {
                   if (navpage != item) {
                     return (
                       <a href={`#${item}Section`}>
-                        <li className="pt-2" key={index}>
+                        <li
+                          className="pt-2 hover:text-gray-200 active:text-gray-500"
+                          key={index}
+                        >
                           {item}
                         </li>
                       </a>
