c9a47306d523ea95bb4dc593fd52badebc94e3d8
Dillan <dillyferster@gmail.com>
2024-08-09 10:44:53 -0600

    menu update

--- src/components/burgerMenu/index.jsx
+++ src/components/burgerMenu/index.jsx
@@ -15,7 +15,7 @@ const BurgerMenu = () => {
   };
 
   return (
-    <div className="flex z-[1000] justify-end px-16 pt-4 items-center text-[var(--black)] text-[28px] backdrop-blur-lg absolute  w-screen h-screen max-sm:text-[18px] ">
+    <div className="flex z-[1000] justify-end px-16 pt-4 items-center text-[var(--black)] text-[28px] backdrop-blur-xl absolute  w-screen h-screen max-sm:text-[18px] ">
       <nav className="w-full">
         <motion.ul
           variants={container}
@@ -27,25 +27,25 @@ const BurgerMenu = () => {
             <a href="/">
               <p className="w-full">Home</p>
             </a>
-            <hr className="border border-[var(--black)]" />
+            <hr className="border border-[var(--beige)]" />
           </motion.li>
           <motion.li className="hover:text-[var(--text-hover)]">
             <a href="/projects">
               <p className="w-full">Projects</p>
             </a>
-            <hr className="border border-[var(--black)]" />
+            <hr className="border border-[var(--beige)]" />
           </motion.li>
           <motion.li className="hover:text-[var(--text-hover)]">
             <a href="/about">
               <p className="w-full">About</p>
             </a>
-            <hr className="border border-[var(--black)]" />
+            <hr className="border border-[var(--beige)]" />
           </motion.li>
           <motion.li className="hover:text-[var(--text-hover)]">
             <a href="/contact">
               <p className="w-full">Contact</p>
             </a>
-            <hr className="border border-[var(--black)]" />
+            <hr className="border border-[var(--beige)]" />
           </motion.li>
         </motion.ul>
       </nav>
