3493a0028471cee13e7c63311fa1a88e93257a58
Dillan <dillyferster@gmail.com>
2024-09-06 10:05:26 -0600

    responsive 1 ud

--- src/pages/projectArticleTwo/index.jsx
+++ src/pages/projectArticleTwo/index.jsx
@@ -18,8 +18,8 @@ function ProjectArticleTwo({ burgerOpen }) {
     >
       {/* <Header title="YYC flight deals"></Header> */}
 
-      <article className="flex flex-col px-16 max-w-[100rem] mx-auto pb-[4rem] max-md:px-8 text-[var(--black)] ">
-        <div className="-translate-x-3 w-full mt-[10rem] pb-[2rem]">
+      <article className="flex flex-col px-16 max-w-[100rem] mx-auto pb-[4rem] max-md:px-2 text-[var(--black)] ">
+        <div className=" w-[10rem] mt-[10rem] pb-[2rem]">
           <a href="/projects">
             <button className="flex border rounded-2xl border-[var(--black)] p-1  justify-evenly w-[7rem] items-center text-[18px] text-[var(--black)] hover:text-[var(--text-hover)]">
               <img
@@ -32,13 +32,13 @@ function ProjectArticleTwo({ burgerOpen }) {
           </a>
         </div>
 
-        <section id="the-concept" className="pb-[12rem] mt-[2rem]">
+        <section id="the-concept" className="pb-[12rem] mt-[2rem] ">
           <div className=" flex justify-between items-center max-lg:justify-center max-lg:flex-col">
             <div className=" flex flex-col">
-              <h2 className="text-[48px] font-bold max-sm:text-[28px]">
+              <h2 className="flex text-[48px] font-bold max-sm:text-[28px] max-lg:justify-center">
                 <span className="text-[var(--green)]">YYC</span> Flight Deals
               </h2>
-              <p className="w-[30rem] pt-[4rem] text-[18px] max-sm:text-[16px]  max-sm:w-[15rem]">
+              <p className="w-[30rem] pt-[4rem] text-[18px] max-sm:text-[16px]  max-sm:w-[20rem]  max-sm:text-center">
                 This idea come out of wanting to go somewhere but not knowing
                 exactly where you want to go ! it takes the searching and
                 thinking out of the decision , how fun right ?  this app has no
@@ -52,8 +52,8 @@ function ProjectArticleTwo({ burgerOpen }) {
           </div>
         </section>
         <section className="pb-[15rem] ">
-          <div className="flex flex-col gap-[2rem] justify-center items-center ">
-            <div className="flex text-wrap  p-[1rem] max-w-[70rem] px-8 text-center w-full rounded-2xl border border-[var(--black)] ">
+          <div className="flex flex-col gap-[2rem] justify-center items-center">
+            <div className="flex text-wrap  p-[1rem] max-w-[70rem] px-8 max-sm:px-1 text-center w-full rounded-2xl border border-[var(--black)]">
               <p className="flex text-[var(--beige)] text-[18px] max-sm:text-[16px]  text-wrap">
                 What sets this app apart is its intentional lack of a search
                 function. Instead, it focuses solely on showcasing the most
@@ -81,7 +81,7 @@ function ProjectArticleTwo({ burgerOpen }) {
               </div>
               <div className="w-[20rem]  p-[1.5rem]   max-lg:w-full  ">
                 {" "}
-                <p className="text-[18px] max-sm:text-[16px]  text-[var(--black)] ">
+                <p className="text-[18px] max-sm:text-[16px]  text-[var(--black)] max-sm:text-center ">
                   I set out to showcase how great images can look when arranged
                   in a grid layout. The design is all about consistency and
                   simplicity. Every line in the grid shares the same width,
@@ -95,7 +95,7 @@ function ProjectArticleTwo({ burgerOpen }) {
               </div>
               <div className="w-[20rem] p-[1.5rem] max-lg:w-full  ">
                 {" "}
-                <p className="text-[18px]  max-sm:text-[16px]  text-[var(--black)] ">
+                <p className="text-[18px]  max-sm:text-[16px]  text-[var(--black)] max-sm:text-center">
                   By emphasizing this initial offering, we create an immediate
                   point of interest that's hard to miss. I also wanted to add a
                   playful touch! The "so why not go" typography adds a sense of
@@ -115,13 +115,13 @@ function ProjectArticleTwo({ burgerOpen }) {
             </div>
           </div>
         </section>
-        <section className="pb-[15rem]">
+        <section className="pb-[15rem] ">
           <div className=" flex max-w-[70rem] items-center justify-between max-lg:flex-col max-lg:items-center">
             <div className=" flex flex-col  max-lg:items-center">
               <h2 className="text-[28px] font-bold">
                 From <span className="text-[var(--yellow)]">Scratch</span>
               </h2>
-              <p className="w-[30rem] pt-[4rem] text-[18px] max-sm:text-[16px] max-lg:text-center">
+              <p className="w-[30rem] pt-[4rem] text-[18px] max-sm:text-[16px] max-lg:text-center max-md:w-[20rem]">
                 That's right no pre-made components were used in the making of
                 this site. Every button, card, grid layout, and interactive
                 element you see is the result of custom CSS. I gained invaluable
@@ -130,7 +130,7 @@ function ProjectArticleTwo({ burgerOpen }) {
                 interface that stands out from cookie-cutter designs.
               </p>
             </div>
-            <div className="h-auto  w-[30rem] max-sm:w-[20] max-lg:mt-[2rem]">
+            <div className="h-auto w-[30rem]  max-sm:w-[23rem] max-lg:mt-[2rem]">
               <img
                 className=" border border-[var(--black)] rounded-2xl bg-[var(--card)] shadow-md "
                 src={detailcard}
@@ -139,24 +139,24 @@ function ProjectArticleTwo({ burgerOpen }) {
             </div>
           </div>
         </section>
-        <section className="pb-[2rem]">
+        <section className="pb-[2rem] ">
           <div className=" flex flex-col  justify-center items-center">
             <div>
               <h2 className="text-[28px] font-bold">
                 The <span className="text-[var(--red)]">Code</span>
               </h2>
             </div>
-            <div className=" flex flex-col gap-8 p-8 max-w-[70rem] justify-between max-lg:justify-center border border-[var(--black)] rounded-3xl bg-[var(--card)] mt-4">
-              <div>
+            <div className=" flex flex-col gap-8 p-8 max-w-[70rem] justify-between max-lg:justify-center border border-[var(--black)] rounded-3xl bg-[var(--card)] mt-4 max-sm:p-4">
+              <div className="h-auto ">
                 <img
-                  className=" h-auto border border-[var(--black)] w-[70rem] rounded-2xl bg-[var(--card)] shadow-md max-sm:w-[20]"
+                  className="  border border-[var(--black)] rounded-2xl bg-[var(--card)] shadow-md "
                   src={flight4}
                   alt="detail card"
                 />
               </div>
               <div className=" flex flex-col ">
                 <h3 className="text-[18px] font-bold">Getting the data</h3>
-                <p className="pt-[1rem] text-[18px] text-[var(--beige)]">
+                <p className="pt-[1rem] text-[18px]  max-sm:text-[16px] text-[var(--beige)]">
                   I used React's fetch API to make three different API calls,
                   ensuring all data including flights and photos is live and
                   dynamic, changing based on the selected city. One crucial
@@ -167,7 +167,7 @@ function ProjectArticleTwo({ burgerOpen }) {
               </div>
               <div className=" flex flex-col ">
                 <h3 className="text-[18px] font-bold">Using the data</h3>
-                <p className=" pt-[1rem] text-[18px] text-[var(--beige)]">
+                <p className=" pt-[1rem] text-[18px]  max-sm:text-[16px] text-[var(--beige)]">
                   Flight Information: The primary API call returns an object
                   with the airport code, price, and other flight details.
                   However, it doesn't provide the city name. To solve this, I
@@ -182,9 +182,9 @@ function ProjectArticleTwo({ burgerOpen }) {
                 </p>
               </div>
 
-              <div>
+              <div className="h-auto ">
                 <img
-                  className=" h-auto border border-[var(--black)] w-[70rem] rounded-2xl bg-[var(--card)] shadow-md max-sm:w-[20]"
+                  className=" h-auto border border-[var(--black)] rounded-2xl bg-[var(--card)] shadow-md "
                   src={flight5}
                   alt="detail card"
                 />
@@ -192,7 +192,7 @@ function ProjectArticleTwo({ burgerOpen }) {
 
               <div className=" flex flex-col ">
                 <h3 className="text-[18px] font-bold">Conditional Rendering</h3>
-                <p className=" pt-[1rem] text-[18px] text-[var(--beige)]">
+                <p className=" pt-[1rem] text-[18px]  max-sm:text-[16px] text-[var(--beige)]">
                   Before the flight data arrives, users see a blank loading
                   element. Once the data is fetched, it transitions to show the
                   full flight detail box. This approach ensures a smooth user
@@ -201,7 +201,7 @@ function ProjectArticleTwo({ burgerOpen }) {
               </div>
               <div className=" flex flex-col ">
                 <h3 className="text-[18px] font-bold">Mapping</h3>
-                <p className=" pt-[1rem] text-[18px] text-[var(--beige)]">
+                <p className=" pt-[1rem] text-[18px]  max-sm:text-[16px] text-[var(--beige)]">
                   By storing all data in an array and using the map function, I
                   can dynamically add data into each detail box component for
                   each flight. This use of component mapping allowed me to reuse
@@ -211,20 +211,20 @@ function ProjectArticleTwo({ burgerOpen }) {
               </div>
               <div className=" flex flex-col ">
                 <h3 className="text-[18px] font-bold">useState & useEffect</h3>
-                <p className=" pt-[1rem] text-[18px] text-[var(--beige)]">
+                <p className=" pt-[1rem] text-[18px]  max-sm:text-[16px] text-[var(--beige)]">
                   useState: All the data returned from the API calls is stored
                   in useState hooks. I did this so the new flight data can
                   update everywhere it is being used in the app, ensuring all
                   the components are using the most relevant data.
                 </p>
-                <p className=" pt-[1rem] text-[18px] text-[var(--beige)]">
+                <p className=" pt-[1rem] text-[18px]  max-sm:text-[16px] text-[var(--beige)]">
                   useEffect: This hook was important for handling my API calls.
                   By using useEffect, I ensure that fetch data functions are
                   called each time the flight detail boxes are rendered on page
                   load.
                 </p>
               </div>
-              <div className="flex gap-2">
+              <div className="flex gap-2 max-sm:flex max-sm:justify-between">
                 <div>
                   <a href="https://yyc-flight-deals.vercel.app/" target="blank">
                     <button className="flex border rounded-2xl border-[var(--black)] p-1  justify-evenly w-[9rem] items-center text-[18px] bg-[var(--bg)] text-[var(--black)] hover:text-[var(--text-hover)]">
@@ -256,8 +256,6 @@ function ProjectArticleTwo({ burgerOpen }) {
             </div>
           </div>
         </section>
-
-        <section></section>
       </article>
     </main>
   );
