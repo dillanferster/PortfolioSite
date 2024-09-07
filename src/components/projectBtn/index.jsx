da5767637df79dee7e909833f9e7160a741383f1
Dillan <dillyferster@gmail.com>
2024-09-07 09:37:51 -0600

    resposive home

--- src/components/projectBtn/index.jsx
+++ src/components/projectBtn/index.jsx
@@ -5,18 +5,18 @@ import "./styles.css";
 function Projectbtn() {
   return (
     <div>
-      <div className="text-[var(--black)] w-[19.75rem] rounded-full border-[var(--black)] border p-2 px-4 bg-[var(--bg)] max-lg:w-[12.25rem] max-sm:w-[8rem]">
+      <div className="text-[var(--black)] w-[19.75rem] rounded-full border-[var(--black)] border p-2 px-4 bg-[var(--bg)] max-xl:w-[12.25rem] max-sm:w-[8rem]">
         <a href="/projects">
           <button
             id="projectBtn-box"
-            className="w-full text-[28px]  flex items-center justify-between cursor-pointer hover:text-[var(--text-hover)] rounded-sm max-lg:text-[22px] max-sm:text-[16px]"
+            className="w-full text-[28px]  flex items-center justify-between cursor-pointer hover:text-[var(--text-hover)] rounded-sm max-xl:text-[22px] max-sm:text-[16px]"
           >
             <span>Projects </span>
             <div id="projectBtn-arrow-box">
               <img
                 src={arrow}
                 alt="arrow icon"
-                className="w-[35px] max-sm:w-[25px] "
+                className="w-[35px] max-lg:w-[25px] "
               ></img>
             </div>
           </button>
