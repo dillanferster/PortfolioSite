360ffad6f52cbfec7bb86eb5d63fe015e43c9905
Dillan <dillyferster@gmail.com>
2024-11-07 17:08:25 -0700

    d

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -72,7 +72,7 @@ const SelectButton = ({ activeTab, setActiveTab, burgerOpen }) => {
         }`}
         onClick={() => setActiveTab("development")}
       >
-        Web Development
+        Web Dev
       </button>
       <button
         className={`px-6 py-2 rounded-full border-2 border-[var(--black)] ${
